ce9ae1a7a4d76a805800c663f941d8a72bbf727f
proskurekov <johndoe@example.com>
2025-10-04 13:50:37 +0700

    update tests

--- src/test/java/hexlet/code/app/controller/api/UserControllerTest.java
+++ src/test/java/hexlet/code/app/controller/api/UserControllerTest.java
@@ -3,12 +3,11 @@ package hexlet.code.app.controller.api;
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import hexlet.code.app.dto.UserUpdateDTO;
 import hexlet.code.app.model.User;
 import hexlet.code.app.repository.UserRepository;
 import hexlet.code.app.util.ModelGenerator;
@@ -17,6 +16,7 @@ import java.util.Map;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openapitools.jackson.nullable.JsonNullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -80,20 +80,54 @@ class UserControllerTest {
     @WithMockUser
     public void testPartialUpdate() throws Exception {
         userRepository.save(testUser);
-        var data = Map.of("firstName", "Alex");
+
+        var updateDto = new UserUpdateDTO();
+        updateDto.setFirstName(JsonNullable.of("Dmitry"));
+
         var request = put("/api/users/" + testUser.getId()).contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(data));
+                .content(objectMapper.writeValueAsString(updateDto));
 
         var response = mockMvc.perform(request).andExpect(status().isOk()).andReturn();
 
         var body = response.getResponse().getContentAsString();
-        assertThatJson(body).and(v -> v.node("firstName").isEqualTo("Alex"),
+        assertThatJson(body).and(v -> v.node("firstName").isEqualTo("Dmitry"),
                 v -> v.node("lastName").isEqualTo(testUser.getLastName()));
 
         var user = userRepository.findById(testUser.getId()).get();
-        assertThat(user.getFirstName()).isEqualTo("Alex");
+        assertThat(user.getFirstName()).isEqualTo("Dmitry");
         assertThat(user.getLastName()).isEqualTo(testUser.getLastName());
         assertThat(user.getUpdatedAt()).isEqualTo(user.getCreatedAt());
     }
 
+    @Test
+    @WithMockUser
+    public void testDestroy() throws Exception {
+        userRepository.save(testUser);
+
+        mockMvc.perform(delete("/api/users/" + testUser.getId()))
+                .andExpect(status().isNoContent());
+
+        assertThat(userRepository.findById(testUser.getId())).isEmpty();
+    }
+
+    @Test
+    @WithMockUser
+    public void testShow() throws Exception {
+        userRepository.save(testUser);
+        var response = mockMvc.perform(get("/api/users/" + testUser.getId())).andExpect(status().isOk())
+                .andReturn();
+        var body = response.getResponse().getContentAsString();
+
+        assertThatJson(body).and(v -> v.node("id").isEqualTo(testUser.getId()),
+                v -> v.node("firstName").isEqualTo(testUser.getFirstName()),
+                v -> v.node("lastName").isEqualTo(testUser.getLastName()),
+                v -> v.node("email").isEqualTo(testUser.getEmail())
+        );
+    }
+
+    @Test
+    public void testShowUnauthorized() throws Exception {
+        userRepository.save(testUser);
+        mockMvc.perform(get("/api/users/" + testUser.getId())).andExpect(status().isUnauthorized());
+    }
 }
